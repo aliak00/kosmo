87d876dd85a05d43a7fd9f8b26a31a6a5e54b811
Ali Akhtarzada <ali@comoyo.com>
2014-12-18 13:10:21 +0100

    Setup up project for ability to inject outputs of dependencies

--- examples/project1.js
+++ examples/project1.js
@@ -1,37 +1,67 @@
 var novaform = require('novaform')
-    , novastl = require('novastl');
+    , novastl = require('novastl')
+    , _ = require('underscore');
 
-module.exports = function(region) {
-    var config = require('./config')(region);
+var stack1 = {
+    name: 'stack1',
+    build: function(options) {
+        var region = options.region;
+        var config = require('./config')(region);
 
-    var vpc = novastl.Vpc({
-        cidr: config.vpcCidrBlock,
-        publicSubnets: config.publicSubnets,
-        privateSubnets: config.privateSubnets
-    });
+        var vpc = novastl.Vpc({
+            cidr: config.vpcCidrBlock,
+            publicSubnets: config.publicSubnets,
+            privateSubnets: config.privateSubnets
+        });
 
-    var nat = novastl.Nat({
-        vpc: vpc,
-        allowedSshCidr: '0.0.0.0/0',
-        keyName: 'test-key-pair',
-        imageId: config.genericImageId,
-        instanceType: 't2.micro'
-    });
+        var nat = novastl.Nat({
+            vpc: vpc,
+            allowedSshCidr: '0.0.0.0/0',
+            keyName: 'test-key-pair',
+            imageId: config.genericImageId,
+            instanceType: 't2.micro'
+        });
 
-    return {
-        stack1: {
+        var privateSubnetRefs = _.map(vpc.refs.private, function(az) {
+            return az.subnet;
+        });
+
+        var vpcPrivateSubnetOutput = novaform.Output('privateSubnets', {
+            Value: novaform.join(',', privateSubnetRefs),
+            Description: 'Private subnets from ' + vpc.name
+        });
+
+        return {
             resourceGroups: [
-                vpc.toResourceGroup()
-            ]
-        },
-
-        stack2: {
-            resourceGroups: [
+                vpc.toResourceGroup(),
                 nat.toResourceGroup()
             ],
-            dependencies: [
-                'stack1'
+
+            outputs: [
+                vpcPrivateSubnetOutput
             ]
-        }
+        };
     }
 };
+
+var stack2 = {
+    name: 'stack2',
+    depends: [stack1],
+    build: function(options) {
+        var region = options.region;
+        var config = require('./config')(region);
+        var subnets = options.inputs.privateSubnets.split(',');
+
+        var rds = novastl.Rds({
+            subnets: subnets
+        });
+
+        return {
+            resourceGroups: [
+                rds
+            ]
+        };
+    }
+};
+
+module.exports = [stack1, stack2];
