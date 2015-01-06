076f26a0e69a72e7a5b4b4bda6e4fe67c0dddf97
Ali Akhtarzada <ali@comoyo.com>
2015-01-06 18:04:55 +0100

    Fix NAT user data script to actually set routes

--- novastl/lib/nat-user-data.sh
+++ novastl/lib/nat-user-data.sh
@@ -18,6 +18,33 @@ error() {
 }
 trap 'error ${LINENO}' ERR
 function log { logger -t "nat" -- $1; }
+
+log "Beginning Port Address Translator (PAT) configuration..."
+log "Determining the MAC address on eth0..."
+ETH0_MAC=$(cat /sys/class/net/eth0/address) ||
+    die "Unable to determine MAC address on eth0."
+log "Found MAC ${ETH0_MAC} for eth0."
+
+VPC_CIDR_URI="http://169.254.169.254/latest/meta-data/network/interfaces/macs/${ETH0_MAC}/vpc-ipv4-cidr-block"
+log "Metadata location for vpc ipv4 range: ${VPC_CIDR_URI}"
+
+VPC_CIDR_RANGE=$(curl --retry 3 --silent --fail ${VPC_CIDR_URI})
+if [ $? -ne 0 ]; then
+   log "Unable to retrive VPC CIDR range from meta-data, using 0.0.0.0/0 instead. PAT may be insecure!"
+   VPC_CIDR_RANGE="0.0.0.0/0"
+else
+   log "Retrieved VPC CIDR range ${VPC_CIDR_RANGE} from meta-data."
+fi
+
+log "Enabling PAT..."
+sysctl -q -w net.ipv4.ip_forward=1 net.ipv4.conf.eth0.send_redirects=0 && (
+   iptables -t nat -C POSTROUTING -o eth0 -s ${VPC_CIDR_RANGE} -j MASQUERADE 2> /dev/null ||
+   iptables -t nat -A POSTROUTING -o eth0 -s ${VPC_CIDR_RANGE} -j MASQUERADE ) ||
+       error
+
+sysctl net.ipv4.ip_forward net.ipv4.conf.eth0.send_redirects | log
+iptables -n -t nat -L POSTROUTING | log
+
 yum update -y aws-cfn-bootstrap aws-cli
 # Set AWS CLI default Region
 region="{{ "Ref" : "AWS::Region" }}"
