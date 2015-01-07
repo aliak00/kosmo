17496a53671d26092d790680b96f4b9b78aea515
Ali Akhtarzada <ali@comoyo.com>
2015-01-07 15:58:20 +0100

    Localize config for nova project examples

--- examples/nova-projects/project1.js
+++ examples/nova-projects/project1.js
@@ -1,12 +1,48 @@
 var _ = require('underscore');
 
+var publicSubnetsMap = {
+    'eu-west-1': {
+        'eu-west-1a': '10.42.1.0/24',
+        'eu-west-1b': '10.42.2.0/24'
+    },
+    'eu-central-1': {
+        'eu-central-1a': '10.42.1.0/24',
+        'eu-central-1b': '10.42.2.0/24'
+    },
+};
+
+var privateSubnetsMap = {
+    'eu-west-1': {
+        'eu-west-1a': '10.42.3.0/24',
+        'eu-west-1b': '10.42.4.0/24'
+    },
+    'eu-central-1': {
+        'eu-central-1a': '10.42.3.0/24',
+        'eu-central-1b': '10.42.4.0/24'
+    },
+};
+
+var images = {
+    'eu-west-1': 'ami-748e2903',
+    'eu-central-1': 'ami-b43503a9'
+};
+
+function makeConfig(region){
+    return {
+        vpcCidrBlock: '10.42.0.0/16',
+        publicSubnetsPerAz: publicSubnetsMap[region],
+        privateSubnetsPerAz: privateSubnetsMap[region],
+        genericImageId: images[region]
+    }
+}
+
 module.exports = function(novaform, novastl) {
 
     var stack1 = {
         name: 'stack1',
         region: 'eu-central-1',
         build: function(dependencies) {
-            var config = require('./config')(this.region);
+            var config = makeConfig(this.region);
 
             var vpc = novastl.Vpc({
                 cidr: config.vpcCidrBlock,
