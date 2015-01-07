80147cfb0316ab0051061bc01f543ffd1e9d342d
Denis Dzyubenko <ddenis@comoyo.com>
2015-01-07 13:33:37 +0100

    Made Output more convenient to use
    It accepts either a key and value as two constructor arguments or key and
    options.

--- novaform/lib/output.js
+++ novaform/lib/output.js
@@ -1,9 +1,11 @@
 var _ = require('underscore');
 
-function Output(name, options) {
+function Output(name, valueOrOptions) {
     if (!(this instanceof Output)) {
-        return new Output(name, options);
+        return new Output(name, valueOrOptions);
     }
+
+    var options = (typeof valueOrOptions === 'object' && 'Value' in valueOrOptions) ? valueOrOptions : { Value : valueOrOptions };
 
     this.name = name;
     this.options = options;
