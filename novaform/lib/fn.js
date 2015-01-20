28f631a8676416cf76918e75b146de94ff659cac
Denis Dzyubenko <ddenis@comoyo.com>
2015-01-20 13:51:48 +0100

    Allow result of functions like Ref in the Output

--- novaform/lib/fn.js
+++ novaform/lib/fn.js
@@ -1,47 +1,78 @@
 var Resource = require('./resource');
 
-function ref(ref) {
-    if (ref instanceof Resource) {
-        return {Ref: ref.name}
+function Function() {
+    if (!(this instanceof Function)) {
+        return new Function();
     }
-    return {Ref: ref}
 }
 
-function join(seperator, values) {
+function Ref(resource) {
+    if (!(this instanceof Ref)) {
+        return new Ref(resource);
+    }
+
+    Function.call(this);
+
+    if (resource instanceof Resource) {
+        this.Ref = resource.name;
+    } else {
+        this.Ref = resource;
+    }
+}
+Ref.prototype = Object.create(Function.prototype);
+
+function Join(separator, values) {
+    if (!(this instanceof Join)) {
+        return new Join(separator, values);
+    }
+
+    Function.call(this);
+
     var modifiedValues = [];
     values.forEach(function(value) {
         if (value instanceof Resource) {
-            modifiedValues.push(ref(value));
+            modifiedValues.push(Ref(value));
         } else {
             modifiedValues.push(value);
         }
     });
-    return {
-        'Fn::Join': [
-            seperator,
-            modifiedValues
-        ]
-    };
-}
 
-function base64(data) {
-    return {
-        'Fn::Base64': data
-    };
+    this['Fn::Join'] = [
+        separator,
+        modifiedValues
+    ];
 }
+Join.prototype = Object.create(Function.prototype);
 
-function getAtt(id, attr) {
-    return {
-        'Fn::GetAtt': [
-            id,
-            attr
-        ]
-    };
+function Base64(data) {
+    if (!(this instanceof Base64)) {
+        return new Base64(data);
+    }
+
+    Function.call(this);
+
+    this['Fn::Base64'] = data;
 }
+Base64.prototype = Object.create(Function.prototype);
+
+function GetAtt(id, attr) {
+    if (!(this instanceof GetAtt)) {
+        return new GetAtt(id, attr);
+    }
+
+    Function.call(this);
+
+    this['Fn::GetAtt'] = [
+        id,
+        attr
+    ];
+}
+GetAtt.prototype = Object.create(Function.prototype);
 
 module.exports = {
-    ref: ref,
-    join: join,
-    base64: base64,
-    getAtt: getAtt
+    Function: Function,
+    ref: Ref,
+    join: Join,
+    base64: Base64,
+    getAtt: GetAtt
 };
--- novaform/lib/output.js
+++ novaform/lib/output.js
@@ -1,12 +1,13 @@
-var Resource = require('./resource');
+var Resource = require('./resource')
+    , fn = require('./fn');
 
 function Output(name, value, description) {
     if (!(this instanceof Output)) {
         return new Output(name, value, description);
     }
 
-    if (!(value instanceof Resource) && !(typeof value === 'string')) {
-        throw new Error('Output value can only be a string or another resource');
+    if (!(value instanceof Resource) && !(value instanceof fn.Function) && !(typeof value === 'string')) {
+        throw new Error('Output value can only be a string or another resource or a join, getatt or an other function');
     }
     if (description && !(typeof description !== 'string')) {
         throw new Error('Output description must be string');
