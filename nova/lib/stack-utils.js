bb9a70309afa2e9a3b9cf01431cdf0fa29a67a03
Denis Dzyubenko <ddenis@comoyo.com>
2015-01-29 12:10:09 +0100

    Refactored stack-utils into a proper Stack class

--- nova/lib/stack-utils.js
+++ nova/lib/stack-utils.js
@@ -1,5 +1,4 @@
-var AWS = require('aws-sdk')
-    , _ = require('underscore');
+var _ = require('underscore');
 
 var Status = {
     DOES_NOT_EXIST: '__DOES_NOT_EXIST',
@@ -22,78 +21,82 @@ var Status = {
     UPDATE_ROLLBACK_IN_PROGRESS: 'UPDATE_ROLLBACK_IN_PROGRESS',
 };
 
-module.exports = {
-    Status: Status,
+function Stack(stackName) {
+    this.name = stackName;
+}
 
-    getStackOutput: function(cfn, stackName, callback) {
-        cfn.describeStacks({
-            StackName: stackName
-        }, function(err, data) {
-            if (err) {
-                if (err.code === 'ValidationError' && err.message.indexOf('does not exist') !== -1) {
-                    callback(Status.DOES_NOT_EXIST);
-                    return;
-                }
-                callback(err);
+Stack.Status = Status;
+
+Stack.getStackInfo = function(cfn, stackName, callback) {
+    cfn.describeStacks({
+        StackName: stackName
+    }, function(err, data) {
+        if (err) {
+            if (err.code === 'ValidationError' && err.message.indexOf('does not exist') !== -1) {
+                callback(Status.DOES_NOT_EXIST);
                 return;
             }
-            var stack = data.Stacks[0];
-            var outputs = _.object(_.map(stack.Outputs, function(e) {
-                return [ e.OutputKey, e.OutputValue ];
-            }));
-            callback(null, outputs);
-        });
-    },
+            callback(err);
+            return;
+        }
+        var stack = data.Stacks[0];
 
-    getStackStatus: function(cfn, stackName, callback) {
-        cfn.describeStacks({
-            StackName: stackName
-        }, function(err, data) {
-            if (err) {
-                if (err.code === 'ValidationError' && err.message.indexOf('does not exist') !== -1) {
-                    callback(null, Status.DOES_NOT_EXIST);
-                    return;
-                }
-                callback(err);
-                return;
-            }
-            var stack = data.Stacks[0];
-            callback(null, stack.StackStatus);
-        });
-    },
-
-    isStatusPending: function(status) {
-        return status === Status.CREATE_IN_PROGRESS ||
-            status === Status.DELETE_IN_PROGRESS ||
-            status === Status.ROLLBACK_IN_PROGRESS ||
-            status === Status.UPDATE_COMPLETE_CLEANUP_IN_PROGRESS ||
-            status === Status.UPDATE_IN_PROGRESS ||
-            status === Status.UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS ||
-            status === Status.UPDATE_ROLLBACK_IN_PROGRESS;
-    },
-
-    isStatusComplete: function(status) {
-        return status === Status.CREATE_COMPLETE ||
-            status === Status.DELETE_COMPLETE ||
-            status === Status.ROLLBACK_COMPLETE ||
-            status === Status.UPDATE_COMPLETE ||
-            status === Status.UPDATE_ROLLBACK_COMPLETE;
-    },
-
-    isStatusFailed: function(status) {
-        return status === Status.CREATE_FAILED ||
-            status === Status.DELETE_FAILED ||
-            status === Status.ROLLBACK_FAILED ||
-            status === Status.UPDATE_ROLLBACK_FAILED;
-    },
-
-    isStatusRolledBack: function(status) {
-        return status === Status.ROLLBACK_COMPLETE ||
-            status === Status.UPDATE_ROLLBACK_COMPLETE;
-    },
-
-    isStatusRollingback: function(status) {
-        return status === Status.ROLLBACK_IN_PROGRESS;
-    },
-
+        var result = new Stack(stackName);
+        result.status = stack.StackStatus;
+        result.outputs = _.object(_.map(stack.Outputs, function(e) {
+            return [ e.OutputKey, e.OutputValue ];
+        }));
+        callback(null, result);
+    });
 };
+
+Stack.getStackOutput = function(cfn, stackName, callback) {
+    this.getStackInfo(cfn, stackName, function(err, stack) {
+        callback(err, stack ? stack.outputs : undefined);
+    });
+};
+
+Stack.getStackStatus = function(cfn, stackName, callback) {
+    this.getStackInfo(cfn, stackName, function(err, stack) {
+        if (err === Status.DOES_NOT_EXIST) {
+            return callback(null, Status.DOES_NOT_EXIST);
+        }
+        callback(err, stack ? stack.status : undefined);
+    });
+};
+
+Stack.isStatusPending = function(status) {
+    return status === Status.CREATE_IN_PROGRESS ||
+        status === Status.DELETE_IN_PROGRESS ||
+        status === Status.ROLLBACK_IN_PROGRESS ||
+        status === Status.UPDATE_COMPLETE_CLEANUP_IN_PROGRESS ||
+        status === Status.UPDATE_IN_PROGRESS ||
+        status === Status.UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS ||
+        status === Status.UPDATE_ROLLBACK_IN_PROGRESS;
+};
+
+Stack.isStatusComplete = function(status) {
+    return status === Status.CREATE_COMPLETE ||
+        status === Status.DELETE_COMPLETE ||
+        status === Status.ROLLBACK_COMPLETE ||
+        status === Status.UPDATE_COMPLETE ||
+        status === Status.UPDATE_ROLLBACK_COMPLETE;
+};
+
+Stack.isStatusFailed = function(status) {
+    return status === Status.CREATE_FAILED ||
+        status === Status.DELETE_FAILED ||
+        status === Status.ROLLBACK_FAILED ||
+        status === Status.UPDATE_ROLLBACK_FAILED;
+};
+
+Stack.isStatusRolledBack = function(status) {
+    return status === Status.ROLLBACK_COMPLETE ||
+        status === Status.UPDATE_ROLLBACK_COMPLETE;
+};
+
+Stack.isStatusRollingback = function(status) {
+    return status === Status.ROLLBACK_IN_PROGRESS;
+};
+
+module.exports = Stack;
