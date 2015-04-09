820736771ddf1835409bcb6a46b5f9124a624f1d
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2015-04-09 16:16:44 +0200

    add --version to nova

--- nova/lib/index.js
+++ nova/lib/index.js
@@ -1,5 +1,16 @@
-var commands = require('./commands');
+var commands = require('./commands')
+    , fs = require('fs')
+    , path = require('path');
 
-module.exports = {
-    commands: commands,
-};
+var nova = {};
+nova.commands = commands;
+
+try {
+    var packagPath = path.join(__dirname, '../package.json');
+    var packageJson = JSON.parse(fs.readFileSync(packagPath));
+    nova.version = packageJson.version;
+} catch (e) {
+    console.log('Error reading verison out of package.json - ', e);
+    nova.version = e;
+}
+module.exports = nova;
