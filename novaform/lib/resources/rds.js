be1dca4a6177bf8df801699bdc469185117deaa0
Denis Dzyubenko <ddenis@comoyo.com>
2015-05-14 11:37:40 +0200

    Switched RDS to new AWSResource class

--- novaform/lib/resources/rds.js
+++ novaform/lib/resources/rds.js
@@ -1,24 +1,88 @@
-var Resource = require('../resource');
+var AWSResource = require('../awsresource')
+    , types = require('../types');
 
-function DBSubnetGroup(name, properties) {
-    if (!(this instanceof DBSubnetGroup)) {
-        return new DBSubnetGroup(name, properties);
-    }
+var DBSubnetGroup = AWSResource.define('AWS::RDS::DBSubnetGroup', {
+    DBSubnetGroupDescription : { type: types.string, required: true },
+    SubnetIds : { type: types.array, required: true },
+    Tags : { type: types.tags },
+});
 
-    Resource.call(this, 'AWS::RDS::DBSubnetGroup', name, properties);
-}
-DBSubnetGroup.prototype = Object.create(Resource.prototype);
+var DBInstance = AWSResource.define('AWS::RDS::DBInstance', {
+    AllocatedStorage : { type: types.number, required: true },
+    AllowMajorVersionUpgrade : { type: types.boolean },
+    AutoMinorVersionUpgrade : { type: types.boolean },
+    AvailabilityZone : { type: types.string },
+    BackupRetentionPeriod : { type: types.number },
+    CharacterSetName : { type: types.string },
+    DBInstanceClass : { type: types.string, required: true },
+    DBInstanceIdentifier : { type: types.string },
+    DBName : { type: types.string },
+    DBParameterGroupName : { type: types.string },
+    DBSecurityGroups : { type: types.array },
+    DBSnapshotIdentifier : { type: types.string },
+    DBSubnetGroupName : { type: types.string },
+    Engine : { type: types.string, required: 'conditional' },
+    EngineVersion : { type: types.string },
+    Iops : { type: types.number, required: 'conditional' },
+    KmsKeyId : { type: types.string },
+    LicenseModel : { type: types.string },
+    MasterUsername : { type: types.string, required: 'conditional' },
+    MasterUserPassword : { type: types.string, required: 'conditional' },
+    MultiAZ : { type: types.boolean },
+    OptionGroupName : { type: types.string },
+    Port : { type: types.string },
+    PreferredBackupWindow : { type: types.string },
+    PreferredMaintenanceWindow : { type: types.string },
+    PubliclyAccessible : { type: types.boolean },
+    SourceDBInstanceIdentifier : { type: types.string },
+    StorageEncrypted : { type: types.boolean, required: 'conditional' },
+    StorageType : { type: types.string },
+    Tags : { type: types.tags },
+    VPCSecurityGroups : { type: types.array },
+});
 
-function DBInstance(name, properties) {
-    if (!(this instanceof DBInstance)) {
-        return new DBInstance(name, properties);
-    }
+var DBParameterGroup = AWSResource.define('AWS::RDS::DBParameterGroup', {
+    Description : { type: types.string, required: true },
+    Family : { type: types.string, required: true },
+    Parameters : { type: types.object },
+    Tags : { type: types.tags },
+});
 
-    Resource.call(this, 'AWS::RDS::DBInstance', name, properties);
-}
-DBInstance.prototype = Object.create(Resource.prototype);
+var RDSSecurityGroupRule = types.object('rds-security-group-rule', {
+    CIDRIP: types.string,
+    EC2SecurityGroupId: types.string,
+    EC2SecurityGroupName: types.string,
+    EC2SecurityGroupOwnerId: types.string,
+});
+
+var DBSecurityGroup = AWSResource.define('AWS::RDS::DBSecurityGroup', {
+    EC2VpcId : { type: types.string, required: 'conditional' },
+    DBSecurityGroupIngress : { type: types.array, required: true }, // array of RDSSecurityGroupRule
+    GroupDescription : { type: types.string, required: true },
+    Tags : { type: types.tags },
+});
+
+var DBSecurityGroupIngress = AWSResource.define('AWS::RDS::DBSecurityGroupIngress', {
+    CIDRIP : { type: types.string },
+    DBSecurityGroupName : { type: types.string, required: true },
+    EC2SecurityGroupId : { type: types.string },
+    EC2SecurityGroupName : { type: types.string },
+    EC2SecurityGroupOwnerId : { type: types.string },
+});
+
+var EventSubscription = AWSResource.define('AWS::RDS::EventSubscription', {
+    Enabled : { type: types.boolean },
+    EventCategories : { type: types.array },
+    SnsTopicArn : { type: types.string, required: true },
+    SourceIds : { type: types.array },
+    SourceType : { type: types.string, required: 'conditional' },
+});
 
 module.exports = {
     DBSubnetGroup: DBSubnetGroup,
-    DBInstance: DBInstance
+    DBInstance: DBInstance,
+    DBParameterGroup: DBParameterGroup,
+    DBSecurityGroup: DBSecurityGroup,
+    DBSecurityGroupIngress: DBSecurityGroupIngress,
+    EventSubscription: EventSubscription,
 };
