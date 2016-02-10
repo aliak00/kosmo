e62b38eeb77d2ee34191e37b344b408cd9862ac9
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2016-02-10 18:43:05 +0100

    modify eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,15 @@
 module.exports = {
-    "rules": {
-        "indent": [
-            2,
-            4
-        ],
-        "quotes": [
-            2,
-            "single"
-        ],
-        "linebreak-style": [
-            2,
-            "unix"
-        ],
-        "semi": [
-            2,
-            "always"
-        ]
+    'rules': {
+        'indent': [2, 4],
+        'quotes': [2, 'single'],
+        'linebreak-style': [2, 'unix'],
+        'semi': [2, 'always'],
+        'comma-dangle': [2, 'always-multiline'],
+        'no-console': 0,
     },
-    "env": {
-        "es6": true,
-        "node": true
+    'env': {
+        'es6': true,
+        'node': true,
     },
-    "extends": "eslint:recommended"
+    'extends': 'eslint:recommended',
 };
