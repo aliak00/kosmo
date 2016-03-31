adbd9071b061f9ee20f8a04cb14408698aa85993
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2016-03-31 21:39:09 +0300

    typo

--- tests/novaform/aws-resource-tests.js
+++ tests/novaform/aws-resource-tests.js
@@ -2,7 +2,7 @@ var AWSResource = require('../../lib/novaform/lib/aws-resource')
 
 describe('AWSResource', function() {
     describe('#define()', function () {
-        it('should return an AWSResource constructir', function () {
+        it('should return an AWSResource constructor', function () {
             const resource = AWSResource.define();
             assert(new resource() instanceof  AWSResource);
         });
