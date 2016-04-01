9323c3c628c8029f0619a5c7e5ee0b216aacf908
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2016-04-01 11:18:54 +0300

    add some more AWSResource.define tests

--- tests/novaform/aws-resource-tests.js
+++ tests/novaform/aws-resource-tests.js
@@ -1,10 +1,26 @@
-var AWSResource = require('../../lib/novaform/lib/aws-resource')
+var AWSResource = require('../../lib/novaform/lib/aws-resource');
 
-describe('AWSResource', function() {
+describe('novaform.AWSResource', function() {
+
     describe('#define()', function () {
-        it('should return an AWSResource constructor', function () {
-            const resource = AWSResource.define();
-            assert(new resource() instanceof  AWSResource);
+
+        it('should return an AWSResource constructor', function() {
+            const Resource = AWSResource.define();
+            assert(typeof Resource === 'function');
+            assert(new Resource() instanceof AWSResource);
         });
+
+        it('should not need new to be constructed', function () {
+            const Resource = AWSResource.define();
+            assert(Resource() instanceof AWSResource);
+        });
+
+        it('should allow aws resource type to be set', function() {
+            const Resource = AWSResource.define('TypeValue');
+            var res = Resource();
+            expect(res).to.have.property('type').and.equal('TypeValue');
+        });
+
     });
+
 });
